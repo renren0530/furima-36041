fda3f92257fadebf69415eb9a000c6d4e51b6197
renren0530 <zoo.zoo.ren@gmail.com>
2021-09-16 15:07:00 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,24 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
+  validates :item_name, presence: true
+  validates :item_info, presence: true
+  validates :item_price, presence: true
+  validates :image, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :item_price, with: PASSWORD_REGEX
+  validates_inclusion_of :item_price, in:300..9999999
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+  validates :item_category_id, numericality: {other_than: 1 , message: "can't be blank"}
+  validates :item_sales_status_id, numericality: {other_than: 1 , message: "can't be blank"}
+  validates :item_shipping_fee_status_id, numericality: {other_than: 1 , message: "can't be blank"}
+  validates :item_prefecture_id , numericality: {other_than: 1 , message: "can't be blank"}
+  validates :item_scheduled_delivery_id, numericality: {other_than: 1 , message: "can't be blank"}
+
 end
