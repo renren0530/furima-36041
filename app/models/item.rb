819bbdf9665b9c022749099769569b859a1c9a93
renren0530 <zoo.zoo.ren@gmail.com>
2021-09-19 12:25:07 +0900

    アソシエーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,11 @@ class Item < ApplicationRecord
  
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :sales_status
-  belongs_to :shipping_fee_status
-  belongs_to :prefecture
-  belongs_to :scheduled_delivery
+  belongs_to :item_category
+  belongs_to :item_sales_status
+  belongs_to :item_shipping_fee_status
+  belongs_to :item_prefecture
+  belongs_to :item_scheduled_delivery
   validates :item_category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :item_sales_status_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :item_shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
